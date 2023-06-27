9391fc8e4f89db48c863594be57b726d9383e033
paul14930 <paul.pelerins@syndicompare.com>
2023-06-27 21:47:53 +0200

    ne plus toucher au migrate

--- db/migrate/20230626143003_create_profiles.rb
+++ db/migrate/20230626143003_create_profiles.rb
@@ -1,6 +1,6 @@
-class CreateProfils < ActiveRecord::Migration[7.0]
+class CreateProfiles < ActiveRecord::Migration[7.0]
   def change
-    create_table :profils do |t|
+    create_table :profiles do |t|
       t.string :username
       t.date :date_birth
       t.string :gender
--- db/migrate/20230626143824_add_score_to_profils.rb
+++
@@ -1,5 +0,0 @@
-class AddScoreToProfils < ActiveRecord::Migration[7.0]
-  def change
-    add_column :profils, :score, :float
-  end
-end
