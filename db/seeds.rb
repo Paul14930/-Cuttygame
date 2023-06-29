8b211edc1ba05ac071b60c8a689b6ea6866248e7
damien palous <palousdamien@gmail.com>
2023-06-29 17:17:54 +0200

    seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,44 +5,112 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-require 'faker'
-require "open-uri"
+# require 'faker'
+# require 'open-uri'
 
-puts "destruction profils"
+# puts 'Destruction profils'
+# Profile.destroy_all
+# puts 'Destruction users'
+# User.destroy_all
+
+# count = 1
+# puts 'Début seed'
+
+# 10.times do
+#   file = File.open(Rails.root.join('seed_images', "H#{count}.jpg"))
+
+#   puts 'Élément en seed Homme'
+#   user = User.create!(email: "user#{count}@example.com", password: "password#{count}")
+
+#   profil = Profile.new(
+#     username: Faker::Name.male_first_name,
+#     date_birth: Faker::Date.birthday(min_age: 18, max_age: 65),
+#     gender: 'Homme',
+#     location: Faker::Address.city,
+#     orientation: 'Homme',
+#     description: Faker::Lorem.paragraphs.join("\n\n"),
+#     user_id: user.id,
+#     score: Faker::Number.between(from: 2000, to: 2500).to_f
+#   )
+
+#   profil.photo.attach(io: file, filename: "H#{count}.jpg", content_type: 'image/jpeg')
+#   profil.save
+
+#   count += 1
+# end
+
+# puts 'Seed terminé'
+
+# # ...
+
+
+# end
+# 10.times do
+#   file = File.open(Rails.root.join('seed_images', "F#{count}.jpg"))
+
+#   puts 'Élément en seed Femme'
+#   user = User.create!(email: "user#{count}@example.com", password: "password#{count}")
+
+#   profil = Profile.new(
+#     username: Faker::Name.female_first_name,
+#     date_birth: Faker::Date.birthday(min_age: 18, max_age: 65),
+#     gender: "Femme",
+#     location: Faker::Address.city,
+#     orientation: "Femme",
+#     description: Faker::Lorem.paragraphs.join("\n\n"),
+#     user_id: user.id,
+#     score: Faker::Number.between(from: 2000, to: 2500).to_f
+#   )
+#   profil.photo.attach(io: file, filename: "F#{count}.jpg", content_type: 'image/jpeg')
+#   profil.save
+
+
+#   count += 1
+
+# end
+# puts "seed fini"
+
+require 'faker'
+
+puts "Destruction profils"
 Profile.destroy_all
-puts "destruction users"
-# Supprimer tous les enregistrements de la table 'users'
+puts "Destruction users"
 User.destroy_all
 
 # ...
+
 count = 1
-puts "debut seed"
-10.times do
-  file = URI.open("https://picsum.photos/200/300?random=1")
-puts "element en seed Homme"
-user = User.create!(email: "user#{count}@example.com", password: "password#{count}")
-  profil = Profile.new(
-    username: Faker::Name.male_first_name,
-    date_birth: Faker::Date.birthday(min_age: 18, max_age: 65),
-    gender: "Homme",
-    location: Faker::Address.city,
-    orientation: "Homme",
-    description: Faker::Lorem.paragraphs.join("\n\n"),
-    user_id: user.id,
-    score: Faker::Number.between(from: 2000, to: 2500).to_f
-  )
-  profil.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-  profil.save
+puts "Début seed"
 
+# 80.times do
+#   puts "Element en seed Homme"
+#   user = User.create!(email: "user#{count}@example.com", password: "password#{count}")
 
-  count += 1
+#   profil = Profile.new(
+#     username: Faker::Name.male_first_name,
+#     date_birth: Faker::Date.birthday(min_age: 18, max_age: 65),
+#     gender: "Homme",
+#     location: Faker::Address.city,
+#     orientation: "Homme",
+#     description: Faker::Lorem.paragraphs.join("\n\n"),
+#     user_id: user.id,
+#     score: Faker::Number.between(from: 2000, to: 2500).to_f
+#     # division: rand(1..6)
+#   )
 
-end
-10.times do
-  file = URI.open("https://picsum.photos/200/300?random=1")
-puts "element en seed Femme"
-user = User.create!(email: "user#{count}@example.com", password: "password#{count}")
-  profil = Profile.new(
+#   file = File.open(Rails.root.join('seed_images', "H#{count}.jpg"))
+#   profil.photo.attach(io: file, filename: "H#{count}.jpg", content_type: 'image/jpeg')
+#   profil.save
+
+#   count += 1
+# end
+# puts "seed finis"
+
+40.times do
+  puts "Element en seed Femme"
+  user = User.create!(email: "user#{count}@example.com", password: "password#{count}")
+
+    profil = Profile.new(
     username: Faker::Name.female_first_name,
     date_birth: Faker::Date.birthday(min_age: 18, max_age: 65),
     gender: "Femme",
@@ -51,12 +119,12 @@ user = User.create!(email: "user#{count}@example.com", password: "password#{coun
     description: Faker::Lorem.paragraphs.join("\n\n"),
     user_id: user.id,
     score: Faker::Number.between(from: 2000, to: 2500).to_f
+    # division: rand(1..6)
   )
-  profil.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-  profil.save
-
+   file = File.open(Rails.root.join('seed_images', "F#{count}.jpg"))
+   profil.photo.attach(io: file, filename: "F#{count}.jpg", content_type: 'image/jpeg')
+   profil.save
 
   count += 1
-
 end
-puts "seed fini"
+puts "seed finis"
