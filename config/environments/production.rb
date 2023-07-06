ff76cb14681ebdeeff892abb9190e47a43276677
paul14930 <paul.pelerins@syndicompare.com>
2023-07-06 17:49:25 +0200

    onespereplus

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
-
+  config.web_socket_server_url = "wss://cutygame.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://cutygame.herokuapp.com', 'http://cutygame.herokuapp.com']
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
