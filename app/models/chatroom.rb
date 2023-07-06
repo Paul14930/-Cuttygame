7a40e5919c15d4d007cfedfb15cd8d952ecf3cf7
gregory bail <gregory.bail@cogeli.fr>
2023-07-06 16:24:27 +0200

    toucheplus

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -4,12 +4,15 @@ class Chatroom < ApplicationRecord
   belongs_to :user_2, class_name: "User"
 
 
+
   def other_user(current_user)
     if current_user == user_1
       user_2
     else
       user_1
     end
+
+
   end
 
 end
