046608b129e0e530052b350212dab0ea9802021a
gregory bail <gregory.bail@cogeli.fr>
2023-07-07 11:59:04 +0200

    fin

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -6,18 +6,21 @@ export default class extends Controller {
   static targets = ["messages", "toto"]
 
   insertMessageAndScrollDown(data) {
-      this.messagesTarget.insertAdjacentHTML("beforeend", data);
-      this.messagesTarget.lastElementChild.scrollIntoView();
-      console.log(this.messagesTarget.scrollHeight);
+    const form = this.element.querySelector("form");
+    form.insertAdjacentHTML("beforebegin", data);
+    form.reset();
+    form.querySelector("textarea").focus();
+    console.log(this.messagesTarget.scrollHeight);
   }
+
   connect() {
     setTimeout(()=> {
       this.messagesTarget.lastElementChild.scrollIntoView({ behavior: "smooth" });
     }, 300);
-    this.channel = createConsumer().subscriptions.create(
-    { channel: "ChatroomChannel", id: this.chatroomIdValue },
-    { received: data => this.insertMessageAndScrollDown(data)}
-  )
-    }
 
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: data => this.insertMessageAndScrollDown(data)}
+    );
+  }
 }
