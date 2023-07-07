906de1020a179fb6690c2cf40f28156dda3571e0
gregory bail <gregory.bail@cogeli.fr>
2023-07-07 12:29:33 +0200

    terminé

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -1,24 +1,24 @@
-import { Controller } from "@hotwired/stimulus"
-import { createConsumer } from "@rails/actioncable"
+import { Controller } from "@hotwired/stimulus";
+import { createConsumer } from "@rails/actioncable";
 
 export default class extends Controller {
-  static values = { chatroomId: Number }
-  static targets = ["messages", "toto"]
+  static values = { chatroomId: Number };
+  static targets = ["messages", "toto"];
 
   insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("afterbegin", data);
-    this.messagesTarget.firstElementChild.scrollIntoView({ behavior: "smooth", block: "start" });
+    this.messagesTarget.insertAdjacentHTML("beforeend", data);
+    this.messagesTarget.lastElementChild.scrollIntoView({ behavior: "smooth" });
     console.log(this.messagesTarget.scrollHeight);
   }
 
   connect() {
-    setTimeout(()=> {
+    setTimeout(() => {
       this.messagesTarget.lastElementChild.scrollIntoView({ behavior: "smooth" });
     }, 300);
 
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.insertMessageAndScrollDown(data)}
+      { received: data => this.insertMessageAndScrollDown(data) }
     );
   }
 }
